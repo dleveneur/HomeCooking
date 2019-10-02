092a3df49a8f0d3e1b9d854f6ebe73735f4ca2f4
rmiranville <ryandelaposs@gmail.com>
2019-10-02 13:54:24 +0400

    permit all

--- HomeCooking/src/main/java/org/HomeCooking/rest/RestApplication.java
+++ HomeCooking/src/main/java/org/HomeCooking/rest/RestApplication.java
@@ -1,8 +1,10 @@
 package org.HomeCooking.rest;
 
 import javax.ws.rs.core.Application;
+import javax.annotation.security.PermitAll;
 import javax.ws.rs.ApplicationPath;
 
+@PermitAll
 @ApplicationPath("/rest")
 public class RestApplication extends Application {
 }
